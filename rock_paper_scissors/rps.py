0c36cd23edaf8ceaee1b483734046a0265eca555
Alex <statueofibberty@gmail.com>
2019-10-09 16:19:47 -0400

    Finish RPS

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,19 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  if n <= 0: # empty array b/c you can't have less than 1
+    return [[]]
+  
+  moves = [["rock"], ["paper"], ["scissors"]]
+  if n == 1:
+    return moves
 
+  final_value = []
+  choices = rock_paper_scissors(n - 1)
+  for item in moves:
+    for value in choices:
+      final_value.append(item + value)
+  return final_value
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
